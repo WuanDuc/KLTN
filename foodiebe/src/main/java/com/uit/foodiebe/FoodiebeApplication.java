ffd890afa473bbbf7952a329d10ae63068055be6
wuan.duc <univerbachtuoc@gmail.com>
2024-12-03 13:21:58 +0700

    init sspring boot'

---
+++ foodiebe/src/main/java/com/uit/foodiebe/FoodiebeApplication.java
@@ -0,0 +1,13 @@
+package com.uit.foodiebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodiebeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodiebeApplication.class, args);
+	}
+
+}
---
+++ foodiebe/src/test/java/com/uit/foodiebe/FoodiebeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uit.foodiebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodiebeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
